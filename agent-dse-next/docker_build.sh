1012896286aa9dd47d47bf8a6052dfc4617d5baf
Yar Khine Phyo <yarkhinephyo@gmail.com>
2023-11-06 14:25:51 -0500

    Support amd64 for dse-next-cdc

--- agent-dse-next/docker_build.sh
+++ agent-dse-next/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker buildx build --platform linux/arm64 . \
-  --push -t yarkhinephyo2019/dse-next:ayman-cdc \
+docker buildx build --platform linux/arm64,linux/amd64 . \
+  --push -t yarkhinephyo2019/dse-next:stargateio-1.0 \
   --build-arg COMMITMOG_SYNC_PERIOD_IN_MS=2000 \
   --build-arg CDC_TOTAL_SPACE_IN_MB=70
